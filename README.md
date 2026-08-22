e829a18e696284b55dc6b35b0f4a5db6f21d5b52
CHAITSNIPER <email>
2026-08-22 21:19:19 +0800

    tcs entered

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a python test  
+This is a python test
---
+++ tests/test_op.py
@@ -0,0 +1,10 @@
+from src.math_opera import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(5,3) == 8
+
+def test_sub():
+    assert sub(5, 3) == 2
+    assert sub(-1, -2) == 1
